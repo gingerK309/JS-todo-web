1343bbafb473557856365caa747a712bd2619da9
gingerK309 <dlatldydid18@naver.com>
2020-07-16 19:53:40 +0900

    calc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const calc={
+    plus: function (a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul: function(a,b){
+        return a*b;
+    },
+    div: function(a,b){
+    return a/b;
+    },
+    pow: function(a,b){
+        return Math.pow(a,b);
+    }
+}
