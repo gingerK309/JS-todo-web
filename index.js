c6bcfbc19202454fb1123b4300c3941510b89704
gingerK309 <dlatldydid18@naver.com>
2020-07-17 21:24:04 +0900

    backgroud

--- index.js
+++ index.js
@@ -17,19 +17,15 @@
 // }
 
 const title = document.querySelector("#title");
-const BASE_COLOR = "darkblue";
-const OTHER_COLOR = "gold";
- function handleClick(){
-     const currentColor = title.style.color;
-     if(currentColor===BASE_COLOR){
-         title.style.color= OTHER_COLOR;
-     }
-     else{
-         title.style.color=BASE_COLOR;
-     }
-} 
+const CLICKED_CLASS ="clicked";
+
+function handleClick(){
+   title.classList.toggle(CLICKED_CLASS);
+}
+
+
 
 function init(){
- title.addEventListener("mouseenter",handleClick); 
+ title.addEventListener("click",handleClick); 
 }
 init();
