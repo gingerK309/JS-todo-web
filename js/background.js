46949787aa7d4abce509e83e06fce228d395fd9c
gingerK309 <49302989+gingerK309@users.noreply.github.com>
2022-11-04 15:34:30 +0900

    Delete js directory

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const background = new Array(6).fill().map((value, index) => {
-  return `${index}.jpg`;
-});
-const selectBg = background[Math.floor(Math.random() * background.length)];
-const bgImg = document.createElement("img");
-bgImg.src = `img/${selectBg}`;
-
-const container = document.querySelector(".container");
-container.appendChild(bgImg);
--- js/quotes_api.js
+++
@@ -1,8 +0,0 @@
-const quotes = document.querySelector(".quotes");
-//API 이용
-const URL = "https://api.adviceslip.com/advice";
-fetch(URL)
-  .then((response) => response.json())
-  .then((quote) => {
-    quotes.innerText = `- ${quote.slip.advice}`;
-  });
